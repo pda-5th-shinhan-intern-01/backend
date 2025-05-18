730593289ff890dff3c8ba4fc8a5395db3013e62
0321minji <0321minji@ewhain.net>
2025-05-18 20:42:07 +0900

    chore : remove unused

--- src/main/java/shinhan/intern/hotsignal/stock/StockController.java
+++ src/main/java/shinhan/intern/hotsignal/stock/StockController.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import shinhan.intern.hotsignal.stock.dto.StockChartDTO;
-import shinhan.intern.hotsignal.stock.dto.StockPriceDTO;
 
 import java.util.List;
 
