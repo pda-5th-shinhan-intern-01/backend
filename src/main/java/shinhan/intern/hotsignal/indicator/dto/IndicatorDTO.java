49aaf2b76739d90db597077797cb72c204070733
0321minji <0321minji@ewhain.net>
2025-05-19 10:08:43 +0900

    feat(#6) : create get all indicator data list

---
+++ src/main/java/shinhan/intern/hotsignal/indicator/dto/IndicatorDTO.java
@@ -0,0 +1,20 @@
+package shinhan.intern.hotsignal.indicator.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class IndicatorDTO {
+    private String name;
+    private String code;
+    private LocalDate date;
+    private Double value;
+    private String unit;
+}
